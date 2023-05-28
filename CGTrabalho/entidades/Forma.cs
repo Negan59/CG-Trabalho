4a2bac052d96cd49c5218a21580608c8e21a7902
Your Name <you@example.com>
2023-05-28 18:40:28 -0300

    primeira atualização

---
+++ CGTrabalho/entidades/Forma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG2bimestre.entidades
+{
+    class Forma
+    {
+
+    }
+}
